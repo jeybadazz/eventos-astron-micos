cf35eaa0f60c4f99b840ed943f71fbf69eef0993
Jerrilhio J Oehlers <jerrilhio10@gmail.com>
2026-08-19 14:28:31 -0300

    Add files via upload

--- eventosAstronomicos/main.js
+++ eventosAstronomicos/main.js
@@ -1,25 +1,10 @@
 import chuvaDeMeteoros from "./data/chuvasDeMeteoros.js";
-import {chuvasDeHoje} from "./funcoes/funcoesLogicas.js";
+import {chuvasDeHoje, proximasChuvas} from "./funcoes/funcoesLogicas.js"; 
 
-const dataAtual = new Date();                 //pegando data do sistema
-const DiaAtual = dataAtual.getDate();         //pegando dia do sistema
-const mesAtual = dataAtual.getMonth() + 1;    //pegando mes do sistema
-const anoAtual = dataAtual.getFullYear();     //pegando ano do sistema
+const dataAtual = new Date();               //pegando a data do sistema
 
-let dataInicioFormatada = `${anoAtual}/${mesAtual}/${DiaAtual}`;    //formatando data  aa/mm/dd
-
-    const arrayInicio = chuvaDeMeteoros.map(chuva => {
-    let novaData = `${anoAtual}/${chuva.inicio}`;  //formatando as datas de inicio do array
-    return novaData;
+const chuvasVistasHoje = chuvaDeMeteoros.filter(chuva => {
+    return chuvasDeHoje(chuvaDeMeteoros, dataAtual);
 });
 
-    const arrayFim = chuvaDeMeteoros.map(chuva => {
-    let novaData = `${anoAtual}/${chuva.fim}`;  //formatando as datas de fim do array
-    return novaData;
-});
-
-const chuvasHoje = chuvaDeMeteoros.filter(chuva => {
-    return chuvasDeHoje(chuva)
-});
-
-console.log(chuvasHoje);
+console.log(chuvasVistasHoje)
